c47f0fb465fda9a909d7dd4bc4e6f2c9928ec12d
pavelnosarev <pavelnosarev@outlook.com>
2023-04-14 13:58:38 -0400

    added main

---
+++ src/main/java/com/pn/MortgageCalculator.java
@@ -0,0 +1,36 @@
+package com.pn;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+        public static void main(String[] args) {
+
+            Scanner myScanner = new Scanner(System.in);
+
+
+            System.out.println( "Hello there. I am here to help with your mortgage calculations.");
+
+            System.out.println(" What is your principal amount: ");
+            double p = myScanner.nextDouble();
+
+            System.out.println( "What is the annual interest rate: ");
+            double i = myScanner.nextDouble();
+
+//            System.out.println(" What is the monthly principle:");
+//            float principleNum = myScanner.nextFloat();
+
+//            System.out.println(" What is the monthly interest rate:");
+//            float interestNum = myScanner.nextFloat();
+//
+//            System.out.println( "How long is the Loan for in months");
+//            float lengthNum = myScanner.nextFloat();
+
+
+
+
+
+        }
+
+
+
+}
