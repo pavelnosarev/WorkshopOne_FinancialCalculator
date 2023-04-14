4620498ad91f96209b33fa2b1bf3541d0e4d568b
pavelnosarev <pavelnosarev@outlook.com>
2023-04-14 15:38:52 -0400

    done with mortgage calc

--- src/main/java/com/pn/MortgageCalculator.java
+++ src/main/java/com/pn/MortgageCalculator.java
@@ -2,19 +2,34 @@ package com.pn;
 
 import java.util.Scanner;
 
+
 public class MortgageCalculator {
+        private static final int months = 12;
         public static void main(String[] args) {
 
             Scanner myScanner = new Scanner(System.in);
 
-
+        /// ask for principal, interest, and term ///
             System.out.println( "Hello there. I am here to help with your mortgage calculations.");
 
             System.out.println(" What is your principal amount: ");
-            double p = myScanner.nextDouble();
+            double principle = myScanner.nextDouble();
 
             System.out.println( "What is the annual interest rate: ");
-            double i = myScanner.nextDouble();
+            float annualInterestRate = myScanner.nextFloat() / 100;
+
+            System.out.println( "How many years is your term: ");
+            int  termInYears = myScanner.nextInt();
+            /// formula to follow M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]///
+            float monthlyInterestRate = annualInterestRate / months;
+            int numOfPayments = termInYears * months;
+            double monthlyPayment =  principle * (
+                    (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numOfPayments)))/ ((Math.pow(1 + monthlyInterestRate, numOfPayments))- 1)
+            );
+            /// show the monthly payment
+            double  totalInterest = (monthlyPayment *numOfPayments) - principle;
+            System.out.println("The monthly payment would be  " + monthlyPayment + " with a lifetime interest of  " + totalInterest + ".");
+
 
 //            System.out.println(" What is the monthly principle:");
 //            float principleNum = myScanner.nextFloat();
