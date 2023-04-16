15e9175ee2344b9a4b7a818c8681c02047e03647
pavelnosarev <pavelnosarev@outlook.com>
2023-04-16 19:51:18 -0400

    added class for CD

---
+++ src/main/java/com/pn/CDValue.java
@@ -0,0 +1,22 @@
+package com.pn;
+
+import java.util.Scanner;
+import java.util.scanner
+public class CDValue {
+    private static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
